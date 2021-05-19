49e532849bdf4d90fe05eefdeb2caf6a5d57219d
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-19 23:56:23 +0530

    Added chat feature

---
+++ src/main/java/ChatFeatureClass.java
@@ -0,0 +1,18 @@
+public class ChatFeatureClass {
+jhfdkajlhkjdglk
+    kjdasdkgjlg
+        dgakjakjdshlkg
+    gkasjkagjg
+                kdgjskdgasjhg
+    dgkajadsgjkg
+                        kdgajgakgasklj
+    jadklghgdlkagd
+                                agkdgajdghagd;k
+    kadgjgkad
+            agkjhgdakalg
+    klaghalgdlkgajg;
+
+    public dgakjakjdshlkg getGkasjkagjg() {
+        return gkasjkagjg;
+    }
+}
