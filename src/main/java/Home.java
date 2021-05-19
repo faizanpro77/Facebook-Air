74fd73e49240b73044e3f74c9051fd4b2bffe39b
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-19 23:49:44 +0530

    Implemented the home feature

---
+++ src/main/java/Home.java
@@ -0,0 +1,11 @@
+public class Home {
+    public static void main(String[] args) {
+        soufjdsajhjkhgd
+                jdga hkdjslhd
+                        dasjhghakjh
+                                asdghklahgkj
+                                dfjdkkfhllfdhhf
+                                        akdjfhjfdhkfafd
+                                        dasjkh
+    }
+}
--- src/main/java/facebook.java
+++ src/main/java/facebook.java
@@ -1,14 +1,12 @@
 public class facebook {
     public static void main(String[] args) {
-        int[] age = {12, 4, 5};
+             int[] age = {12, 4, 5};
 
-        // loop through the array
-        // using for loop
-        System.out.println("Using for-each Loop:");
-        for(int a : age) {
+
+                   System.out.println("Using for-each Loop:");
+        for (int a : age) {
             System.out.println(a);
+
         }
-
-
     }
 }
